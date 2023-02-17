a3a6ccb150c2c3a62dc760cdfa4b7068c69e5a30
TikTak <guplik@bk.ru>
2023-02-17 10:17:27 +0300

    Добавьте файлы проекта.

---
+++ Labs WinForms/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Labs_WinForms
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 101);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+10";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(477, 101);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-10";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar1.ForeColor = System.Drawing.Color.Crimson;
+            this.progressBar1.Location = new System.Drawing.Point(13, 13);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(688, 39);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(360, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.MaximumSize = new System.Drawing.Size(1333, 1231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 3;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(709, 169);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Сумма";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Labs WinForms/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labs_WinForms
+{
+    public partial class Form1 : Form
+    {
+        bool IsGoRight;
+        float time;
+        int t;
+
+        public Form1()
+        {
+            InitializeComponent();      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(progressBar1.Value >= 10)
+                progressBar1.Value -= 10;
+
+            if (progressBar1.Value != 0 | progressBar1.Value != 100)
+                label1.Text = "";
+
+
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            IsGoRight = true;
+
+
+            if (progressBar1.Value == 0 | progressBar1.Value == 100)
+            {
+                label1.Text = time.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value <= 90)
+                progressBar1.Value += 10;
+
+            if (progressBar1.Value != 0 | progressBar1.Value != 100)
+                label1.Text = "";
+            else
+
+            if (progressBar1.Value == 0 | progressBar1.Value == 100)
+            {
+               label1.Text = time.ToString();
+
+            }
+
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            IsGoRight = false;
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time += 0.1f;
+            if (progressBar1.Value == 0 | progressBar1.Value == 100)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                label1.Text = time.ToString();
+
+            }
+        }
+            private void timer2_Tick(object sender, EventArgs e)
+        {
+                if (IsGoRight && progressBar1.Value >= 10)
+                {
+                    progressBar1.Value -= 10;
+
+                }
+                else if (!IsGoRight && progressBar1.Value <= 90)
+                {
+                    progressBar1.Value += 10;
+
+                }
+
+
+                if (progressBar1.Value == 0 | progressBar1.Value == 100)
+                {
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                    label1.Text = time.ToString();
+
+                }
+                else
+                    label1.Text = "";         
+        }
+     
+    }
+}
+
