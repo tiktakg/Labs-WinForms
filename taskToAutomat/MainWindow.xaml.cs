e1b7322aca55b1ac1e0751d49de5856ce90145eb
tiktakg <guplik@bk.ru>
2023-11-18 18:26:23 +0300

    make set color for answer

--- taskToAutomat/MainWindow.xaml.cs
+++ taskToAutomat/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +20,8 @@ namespace taskToAutomat
  
     public partial class MainWindow : Window
     {
+        ObservableCollection<Samples> dataCollection = new ObservableCollection<Samples>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -25,24 +29,62 @@ namespace taskToAutomat
 
         private void MakeNewSamples_Click(object sender, RoutedEventArgs e)
         {
-            gridWithSamples.Items.Add("12", "he");
+            Samples sample = new Samples(1,"test","test1","test2");
+            dataCollection.Add(sample);
+            gridWithSamples.Items.Add(sample);
         }
 
         private void ShowAnswer_Click(object sender, RoutedEventArgs e)
         {
+            var allItems = gridWithSamples.Items;
 
-           
+
+
+            //Samples selectedItem = (Samples)gridWithSamples.hea.SelectedItem;
+
+            Samples itemToModify = dataCollection[0];
+
+            Samples sample = new Samples(5, "t2est", "te2st1", "tes2t2");
+          
+
+
+            int index = 0;
+            foreach (Samples item in allItems)
+            {
+                if (item.Answer != item.TrueAnswer)
+                {
+                    dataCollection[index].Status = false;
+                }
+                else
+                {
+                    dataCollection[index].Status = true;
+                }
+
+                //dataCollection[index];
+                index++;
+            }
+
+            gridWithSamples.Items.Refresh();
 
 
         }
     }
-    public class Customer
+    public class Samples
     {
         public int ID { get; set; }
         public string Sample { get; set; }
         public string Answer { get; set; }
         public string TrueAnswer { get; set; }
-        public bool Status { get; set; }
+        public bool? Status { get; set; }
+
+        public Samples(int id, string sample, string answer, string trueAnswer)
+        {
+            ID = id;
+            Sample = sample;
+            Answer = answer;
+            TrueAnswer = trueAnswer;
+            Status = null;
+        }
     }
 
 }
